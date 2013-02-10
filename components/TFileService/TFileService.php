8d0c1cceb48f1c3195388d18a0ab7f0c5cffa400
ivan <ivan@ivan-EG41MF-US2H>
2013-02-10 17:57:49 +0200

    working example 2

--- components/TFileService/TFileService.php
+++ components/TFileService/TFileService.php
@@ -1,13 +1,14 @@
 <?php
-class TFileService extends TDBService{
+
+class TFileService extends TDBService {
 /*SRVDEF*/
 private static $_definition_struc = array (
   'models' =>array (
     'userfiles' =>array (
       'p' => 'a:3:{s:3:"idx";a:2:{i:0;i:1;i:1;b:0;}s:7:"filters";a:2:{i:0;i:3;i:1;b:0;}s:5:"order";a:2:{i:0;i:3;i:1;b:0;}}',
-      'i' => 'a:7:{s:6:"origin";a:2:{i:0;i:3;i:1;b:0;}s:8:"filesize";a:2:{i:0;i:1;i:1;b:0;}s:8:"filetype";a:2:{i:0;i:3;i:1;b:0;}s:5:"title";a:2:{i:0;i:3;i:1;b:0;}s:4:"text";a:2:{i:0;i:3;i:1;b:0;}s:6:"rights";a:2:{i:0;i:3;i:1;b:0;}s:4:"data";a:2:{i:0;i:3;i:1;b:1;}}',
-      'u' => 'a:8:{s:6:"origin";a:2:{i:0;i:3;i:1;b:0;}s:8:"filetype";a:2:{i:0;i:3;i:1;b:0;}s:4:"date";a:2:{i:0;i:3;i:1;b:0;}s:2:"ip";a:2:{i:0;i:3;i:1;b:0;}s:5:"title";a:2:{i:0;i:3;i:1;b:0;}s:4:"text";a:2:{i:0;i:3;i:1;b:0;}s:5:"owner";a:2:{i:0;i:1;i:1;b:0;}s:6:"rights";a:2:{i:0;i:3;i:1;b:0;}}',
-      'f' => 'a:12:{s:3:"idx";i:1;s:6:"origin";i:3;s:8:"filesize";i:1;s:8:"filetype";i:3;s:5:"size1";i:1;s:5:"size2";i:1;s:4:"date";i:3;s:2:"ip";i:3;s:5:"title";i:3;s:4:"text";i:3;s:5:"owner";i:1;s:6:"rights";i:3;}',
+      'i' => 'a:9:{s:6:"origin";a:2:{i:0;i:3;i:1;b:0;}s:8:"filesize";a:2:{i:0;i:1;i:1;b:0;}s:8:"filetype";a:2:{i:0;i:3;i:1;b:0;}s:9:"extention";a:2:{i:0;i:3;i:1;b:0;}s:5:"title";a:2:{i:0;i:3;i:1;b:0;}s:4:"tags";a:2:{i:0;i:3;i:1;b:0;}s:4:"text";a:2:{i:0;i:3;i:1;b:0;}s:6:"rights";a:2:{i:0;i:3;i:1;b:0;}s:4:"data";a:2:{i:0;i:3;i:1;b:1;}}',
+      'u' => 'a:9:{s:6:"origin";a:2:{i:0;i:3;i:1;b:0;}s:8:"filetype";a:2:{i:0;i:3;i:1;b:0;}s:4:"date";a:2:{i:0;i:3;i:1;b:0;}s:2:"ip";a:2:{i:0;i:3;i:1;b:0;}s:5:"title";a:2:{i:0;i:3;i:1;b:0;}s:4:"tags";a:2:{i:0;i:3;i:1;b:0;}s:4:"text";a:2:{i:0;i:3;i:1;b:0;}s:5:"owner";a:2:{i:0;i:1;i:1;b:0;}s:6:"rights";a:2:{i:0;i:3;i:1;b:0;}}',
+      'f' => 'a:15:{s:3:"idx";i:1;s:6:"origin";i:3;s:8:"filesize";i:1;s:8:"filetype";i:3;s:9:"extention";i:3;s:5:"size1";i:1;s:5:"size2";i:1;s:4:"date";i:3;s:2:"ip";i:3;s:5:"title";i:3;s:4:"tags";i:3;s:4:"text";i:3;s:5:"owner";i:1;s:6:"rights";i:3;s:4:"path";i:3;}',
       'owner' => true,
     ),
   ),
@@ -23,41 +24,98 @@ protected function &getDefinitionStruc(){return self::$_definition_struc;}
 /*SRVDEF*/
     const ACCOUNT_SRV_REQUIRED = 300;
     const USER_REQUIRED = 301;
-    const SQL_DATE_FORMAT = 'Y-m-d h:i:s';
+    const CREATE_DIRECTORY = 302;
+    const FILE_SIZE_LIMIT = 303;
+    const EXT_NOT_ALLOWED = 304;
+    const DBERROR = 305;
 
-    
-    public function createTable($name){
-        $path = $this->project->path.'/data/'.$this->name;
-        if(!file_exists($path)) { if(!mkdir($path)) self::error(self::CREATE_DIRECTORY,$path);}
+    public function createTable($name) {
+        $path = $this->project->path . '/data/' . $this->name;
+        if (!file_exists($path)) {
+            if (!mkdir($path))
+                self::error(self::CREATE_DIRECTORY, $path);
+        }
         parent::createTable($name);
     }
-    
-    public function _fetch_userfiles_model($args){
+
+    public function _fetch_userfiles_model($args) {
         $this->_getSQLWhere($args, array('idx'), $where, $params);
         $r = $this->_fetchTableModel($args, 'userfiles', $where, $params);
         return $r;
     }
-    public function _insert_userfiles_model($args){
+
+    public function _insert_userfiles_model($args) {
         $v = $args['values'];
         $uidx = $this->project->user_id;
-        //if(($uidx == 0) && $this->auth_required) self::error(self::USER_REQUIRED);
+        if (($uidx == 0) && $this->auth_required)
+            self::error(self::USER_REQUIRED);
+        $m_bytes = round(strlen(rtrim($v['data'], '=')) * 3 / 4194304, 2);
+        if ($m_bytes > $this->max_size_MB)
+            self::error(self::FILE_SIZE_LIMIT, $m_bytes . 'MB>' . $this->max_size_MB . 'MB'); //exeption on file_size
         $v['owner'] = $uidx;
-        $v['date'] = date(self::SQL_DATE_FORMAT,time());
-        $v['ip'] =$_SERVER['REMOTE_ADDR'];
+        $v['date'] = date(self::SQL_DATE_FORMAT, time());
+        $v['ip'] = $_SERVER['REMOTE_ADDR'];
+        $v_data = $v['data'];
+        unset($v['data']);
+        $this->file_ext = strtolower(substr(strrchr($v['origin'], '.'), 1));
+        if (array_search($this->file_ext, $this->allowed_ext) === false)
+            self::error(self::EXT_NOT_ALLOWED, $this->file_ext); //exeption on file_ext
+        $v['extention'] = $this->file_ext;
         $this->_insertTableModel($v, 'userfiles');
-        $this->last_id = $this->db->lastInsertId(); 
-        $this->file_ext = substr($v['origin'], strrpos($v['origin'], '.') + 1);
-        //exeption on file_ext
-        list($dir,$fname) = $this->_getFileName();
-        $path = $this->project->path.'/data/'.$this->name.'/'.$dir;
-        if(!file_exists($path)) { if(!mkdir($path)) self::error(self::CREATE_DIRECTORY,$path);}
-        $file = $path.'/'.$fname;
-        file_put_contents ($file, base64_decode($v['data']));
+        $v['data'] = $v_data;
+        $this->last_id = $this->db->lastInsertId();
+        list($dir, $fname) = $this->_getFileName($this->last_id, $this->file_ext);
+        $path = $this->project->path . '/data/' . $this->name . '/' . $dir;
+        if (!file_exists($path)) {
+            if (!mkdir($path))
+                self::error(self::CREATE_DIRECTORY, $path);
+        }
+        $file = $path . '/' . $fname;
+        $fileURL = $dir . '/' . $fname;
+        $table = $this->name.'_userfiles';
+        file_put_contents($file, base64_decode($v['data']));
+        $sql = "UPDATE `$table`  SET path='$fileURL' WHERE idx = '$this->last_id'";
+        if(self::$this->db->query($sql)===false) self::_dbError();
+
     }
-    	protected function _getFileName(){
-		$dir = "R".str_pad(round($this->last_id/100),7, '0', STR_PAD_LEFT);
-		$fname = str_pad($this->last_id,7, '0', STR_PAD_LEFT).".".$this->file_ext;
-		return array($dir,$fname);
-	}
+    public function _remove_userfiles_model($args) {
+        list($dir, $fname) = $this->_getFileName($args['index'],"");
+        //удаляем все файлы, которые начинаются на $fname, т.е. и тумбы и т.д.
+        array_map("unlink", glob($this->project->path . '/data/' . $this->name . '/' . $dir. '/' . $fname."*"));
+        $this->_removeTableModel($args['index'], 'userfiles');
+    }
+
+    protected function _getFileName($id, $ext) {
+        $dir = "R" . str_pad(round($id / 100), 7, '0', STR_PAD_LEFT);
+        $fname = str_pad($id, 7, '0', STR_PAD_LEFT) . "." . $ext;
+        return array($dir, $fname);
+    }
+
+    protected static function _getErrorMsg($args) {
+        $code = $args[0];
+        switch ($code) {
+            case self::CREATE_DIRECTORY: {
+                    $msg = 'Can not create directory: ' . $args[1];
+                    break;
+                }
+            case self::FILE_SIZE_LIMIT: {
+                    $msg = 'File size limit exceeded: ' . $args[1];
+                    break;
+                }
+            case self::EXT_NOT_ALLOWED: {
+                    $msg = 'File extention is not allowed: ' . $args[1];
+                    break;
+                }
+            case self::USER_REQUIRED: {
+                    $msg = "Authorization required";
+                    break;
+                }
+                case self::DBERROR: {$msg = 'Database error: '.$args[1] ;break;}
+            default: $msg = parent::_getErrorMsg($args);
+        }
+        return $msg;
+    }
+
 }
+
 ?>
