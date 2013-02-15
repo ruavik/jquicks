a47cc1044915571de4e7a5707cebff7bed804829
ivan <ivan@ivan-EG41MF-US2H>
2013-02-15 19:19:13 +0200

    file loader component

--- components/TFileService/TFileService.php
+++ components/TFileService/TFileService.php
@@ -1,42 +1,41 @@
 <?php
 
 class TFileService extends TDBService {
-/*SRVDEF*/
-private static $_definition_struc = array (
-  'models' =>array (
-    'userfiles' =>array (
-      'p' => 'a:3:{s:3:"idx";a:2:{i:0;i:1;i:1;b:0;}s:7:"filters";a:2:{i:0;i:3;i:1;b:0;}s:5:"order";a:2:{i:0;i:3;i:1;b:0;}}',
-      'i' => 'a:9:{s:6:"origin";a:2:{i:0;i:3;i:1;b:0;}s:8:"filesize";a:2:{i:0;i:1;i:1;b:0;}s:8:"filetype";a:2:{i:0;i:3;i:1;b:0;}s:9:"extention";a:2:{i:0;i:3;i:1;b:0;}s:5:"title";a:2:{i:0;i:3;i:1;b:0;}s:4:"tags";a:2:{i:0;i:3;i:1;b:0;}s:4:"text";a:2:{i:0;i:3;i:1;b:0;}s:6:"rights";a:2:{i:0;i:3;i:1;b:0;}s:4:"data";a:2:{i:0;i:3;i:1;b:1;}}',
-      'u' => 'a:9:{s:6:"origin";a:2:{i:0;i:3;i:1;b:0;}s:8:"filetype";a:2:{i:0;i:3;i:1;b:0;}s:4:"date";a:2:{i:0;i:3;i:1;b:0;}s:2:"ip";a:2:{i:0;i:3;i:1;b:0;}s:5:"title";a:2:{i:0;i:3;i:1;b:0;}s:4:"tags";a:2:{i:0;i:3;i:1;b:0;}s:4:"text";a:2:{i:0;i:3;i:1;b:0;}s:5:"owner";a:2:{i:0;i:1;i:1;b:0;}s:6:"rights";a:2:{i:0;i:3;i:1;b:0;}}',
-      'f' => 'a:15:{s:3:"idx";i:1;s:6:"origin";i:3;s:8:"filesize";i:1;s:8:"filetype";i:3;s:9:"extention";i:3;s:5:"size1";i:1;s:5:"size2";i:1;s:4:"date";i:3;s:2:"ip";i:3;s:5:"title";i:3;s:4:"tags";i:3;s:4:"text";i:3;s:5:"owner";i:1;s:6:"rights";i:3;s:4:"path";i:3;}',
-      'owner' => true,
-    ),
-  ),
-  'commands' =>array (
-  ),
-  'links' =>array (
-    'userfiles' =>array (
-      'owner' => 'TAccountService.users',
-    ),
-  ),
-);
-protected function &getDefinitionStruc(){return self::$_definition_struc;}
-/*SRVDEF*/
+    /* SRVDEF */
+
+    private static $_definition_struc = array(
+        'models' => array(
+            'userfiles' => array(
+                'p' => 'a:3:{s:3:"idx";a:2:{i:0;i:1;i:1;b:0;}s:7:"filters";a:2:{i:0;i:3;i:1;b:0;}s:5:"order";a:2:{i:0;i:3;i:1;b:0;}}',
+                'i' => 'a:9:{s:6:"origin";a:2:{i:0;i:3;i:1;b:0;}s:8:"filesize";a:2:{i:0;i:1;i:1;b:0;}s:8:"filetype";a:2:{i:0;i:3;i:1;b:0;}s:9:"extention";a:2:{i:0;i:3;i:1;b:0;}s:5:"title";a:2:{i:0;i:3;i:1;b:0;}s:4:"tags";a:2:{i:0;i:3;i:1;b:0;}s:4:"text";a:2:{i:0;i:3;i:1;b:0;}s:6:"rights";a:2:{i:0;i:3;i:1;b:0;}s:4:"data";a:2:{i:0;i:3;i:1;b:1;}}',
+                'u' => 'a:9:{s:6:"origin";a:2:{i:0;i:3;i:1;b:0;}s:8:"filetype";a:2:{i:0;i:3;i:1;b:0;}s:4:"date";a:2:{i:0;i:3;i:1;b:0;}s:2:"ip";a:2:{i:0;i:3;i:1;b:0;}s:5:"title";a:2:{i:0;i:3;i:1;b:0;}s:4:"tags";a:2:{i:0;i:3;i:1;b:0;}s:4:"text";a:2:{i:0;i:3;i:1;b:0;}s:5:"owner";a:2:{i:0;i:1;i:1;b:0;}s:6:"rights";a:2:{i:0;i:3;i:1;b:0;}}',
+                'f' => 'a:15:{s:3:"idx";i:1;s:6:"origin";i:3;s:8:"filesize";i:1;s:8:"filetype";i:3;s:9:"extention";i:3;s:5:"size1";i:1;s:5:"size2";i:1;s:4:"date";i:3;s:2:"ip";i:3;s:5:"title";i:3;s:4:"tags";i:3;s:4:"text";i:3;s:5:"owner";i:1;s:6:"rights";i:3;s:4:"path";i:3;}',
+                'owner' => true,
+            ),
+        ),
+        'commands' => array(
+        ),
+        'links' => array(
+            'userfiles' => array(
+                'owner' => 'TAccountService.users',
+            ),
+        ),
+    );
+
+    protected function &getDefinitionStruc() {
+        return self::$_definition_struc;
+    }
+
+    /* SRVDEF */
+
     const ACCOUNT_SRV_REQUIRED = 300;
     const USER_REQUIRED = 301;
     const CREATE_DIRECTORY = 302;
     const FILE_SIZE_LIMIT = 303;
     const EXT_NOT_ALLOWED = 304;
     const DBERROR = 305;
-
-    public function createTable($name) {
-        $path = $this->project->path . '/data/' . $this->name;
-        if (!file_exists($path)) {
-            if (!mkdir($path))
-                self::error(self::CREATE_DIRECTORY, $path);
-        }
-        parent::createTable($name);
-    }
+    const DEFAULT_DIR_MODE = 0755; //rvk
+    const DEFAULT_FILE_MODE = 0644; //rvk
 
     public function _fetch_userfiles_model($args) {
         $this->_getSQLWhere($args, array('idx'), $where, $params);
@@ -63,25 +62,39 @@ protected function &getDefinitionStruc(){return self::$_definition_struc;}
         $v['extention'] = $this->file_ext;
         $this->_insertTableModel($v, 'userfiles');
         $v['data'] = $v_data;
+        $files_dir = $this->name;
+        $path = $this->project->path . '/data/' . $files_dir;
         $this->last_id = $this->db->lastInsertId();
-        list($dir, $fname) = $this->_getFileName($this->last_id, $this->file_ext);
-        $path = $this->project->path . '/data/' . $this->name . '/' . $dir;
-        if (!file_exists($path)) {
-            if (!mkdir($path))
-                self::error(self::CREATE_DIRECTORY, $path);
-        }
-        $file = $path . '/' . $fname;
-        $fileURL = $dir . '/' . $fname;
-        $table = $this->name.'_userfiles';
-        file_put_contents($file, base64_decode($v['data']));
-        $sql = "UPDATE `$table`  SET path='$fileURL' WHERE idx = '$this->last_id'";
-        if(self::$this->db->query($sql)===false) self::_dbError();
 
+        if ($this->anonim_file_name === 1) {
+            list($dir, $fname) = $this->_getFileName($this->last_id, $this->file_ext);
+            $path.= '/'.$dir;
+            if (!file_exists($path)) {
+                if (!mkdir($path, self::DEFAULT_DIR_MODE, true))
+                    self::error(self::CREATE_DIRECTORY, $path);
+            }
+            $file = $path . '/' . $fname;
+            $fileURL = $dir . '/' . $fname;
+        }
+        else {
+            $file = $path . '/' . $v['origin'];
+            $fileURL = $v['origin'];
+        }
+        $table = $this->name . '_userfiles';
+        if (file_put_contents($file, base64_decode($v['data']))) {
+            $sql = "UPDATE `$table`  SET path='$fileURL' WHERE idx = '$this->last_id'";
+        } else {
+            $sql = "DELETE FROM `$table` WHERE idx = '$this->last_id'";
+        }
+        if (self::$this->db->query($sql) === false)
+            self::_dbError();
     }
+
     public function _remove_userfiles_model($args) {
-        list($dir, $fname) = $this->_getFileName($args['index'],"");
-        //удаляем все файлы, которые начинаются на $fname, т.е. и тумбы и т.д.
-        array_map("unlink", glob($this->project->path . '/data/' . $this->name . '/' . $dir. '/' . $fname."*"));
+        $table = $this->name . '_userfiles';
+        $cmd = $this->_exec("SELECT path FROM `$table` WHERE idx=".$args['index']);
+        $rows = $cmd->fetchAll(PDO::FETCH_ASSOC);
+        array_map("unlink", glob($this->project->path . '/data/' . $this->name . '/' . $rows[0]['path']));
         $this->_removeTableModel($args['index'], 'userfiles');
     }
 
@@ -110,7 +123,10 @@ protected function &getDefinitionStruc(){return self::$_definition_struc;}
                     $msg = "Authorization required";
                     break;
                 }
-                case self::DBERROR: {$msg = 'Database error: '.$args[1] ;break;}
+            case self::DBERROR: {
+                    $msg = 'Database error: ' . $args[1];
+                    break;
+                }
             default: $msg = parent::_getErrorMsg($args);
         }
         return $msg;
--- components/TFileService/TFileService_.php
+++ components/TFileService/TFileService_.php
@@ -1,21 +1,61 @@
 <?php
-class TFileService_ extends TDBService_{
-    const RENAME_PAGE_FILE = 301;
-    
-        protected function _rename($name) {
+
+class TFileService_ extends TDBService_ {
+
+    const RENAME_DIRECTORY = 301;
+    const CREATE_DIRECTORY = 302;
+    const DEFAULT_DIR_MODE = 0755;
+    const DEFAULT_FILE_MODE = 0644;
+
+    public function insertIntoProject($parent, $section, $order, $page, $name = null) {
+        parent::insertIntoProject($parent, $section, $order, $page, $name = null);
+        $files_dir = $this->project->db['components'][$this->id]['n'];
+        $this->makeRepositoryDir($files_dir);
+    }
+
+    public function makeRepositoryDir($files_dir) {
+        $path = $this->project->path . '/data/' . $files_dir;
+        if (file_exists($path))
+            return;
+        if (!file_exists($path)) {
+            if (!mkdir($path, self::DEFAULT_DIR_MODE, true))
+                self::error(self::CREATE_DIRECTORY, $path);
+        }
+    }
+
+    public function renameRepositoryDir($oldname, $newname) {
+        $path = $this->project->path . '/data/';
+        if (file_exists($path . $newname))
+            return; //если случайно удалить файловую службу 
+                    //или компонент со встроенной файловой службой, 
+                    //то таблица базы данных и директория с файлами не удаляются 
+                    //можно создать компонент заново с таким же именем - без потери данных
+
+        if (!rename($path . $oldname, $path . $newname))
+            self::error(self::RENAME_DIRECTORY, $oldname);
+    }
+
+    protected function _rename($name) {
         $oldname = strtolower($this->_getName());
         $newname = strtolower($name);
         parent::_rename($name);
-        $path = $this->project->path;
-        if(!rename($path.'/data/'.$oldname,$path.'/data/'.$newname)) self::error(self::RENAME_PAGE_FILE,$oldname);
-        
+        $this->renameRepositoryDir($oldname, $newname);
     }
-    protected static function _getErrorMsg($args){
+
+    protected static function _getErrorMsg($args) {
         $code = $args[0];
-        switch ($code){
-            case self::RENAME_PAGE_FILE: {$msg = 'Can not rename page file: '.$args[1].'.php'; break;}
+        switch ($code) {
+            case self::CREATE_DIRECTORY: {
+                    $msg = 'Can not create directory: ' . $args[1];
+                    break;
+                }
+            case self::RENAME_DIRECTORY: {
+                    $msg = 'Can not rename directory: ' . $args[1];
+                    break;
+                }
             default: $msg = parent::_getErrorMsg($args);
         }
         return $msg;
-    }    
+    }
+
 }
